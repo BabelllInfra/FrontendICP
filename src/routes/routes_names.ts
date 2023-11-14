dfaef03403966904c80646154caeb92dfa4bbd1a
Hectorc70 <hecg.dev@gmail.com>
2023-11-14 00:44:24 -0600

    init

---
+++ src/routes/routes_names.ts
@@ -0,0 +1,6 @@
+export const routesNamesAuth = {
+  newTransaction: '/',
+  qrTransaction: 'qr/',
+  messageWarningTransaction: 'message-new-transaction/',
+  messageSuccessTransaction: 'message-success-transaction/',
+}
