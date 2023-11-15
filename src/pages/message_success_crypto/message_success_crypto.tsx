a940fa6901085d3f6863efca4092684bfa462a24
Hectorc70 <hecg.dev@gmail.com>
2023-11-15 05:36:45 -0600

    fix

--- src/pages/message_success_crypto/message_success_crypto.tsx
+++ src/pages/message_success_crypto/message_success_crypto.tsx
@@ -12,14 +12,14 @@ const MessageSuccessSendCrypto = () => {
   return (
     <div className="bg-splash h-screen relative h-screen w-screen overflow-auto">
       <div className=" h-full flex w-full flex-col items-center justify-start">
-        <CloseButton onClick={() => navigate(routesNamesApp.qrTransaction)} />
+        <CloseButton onClick={() => navigate(routesNamesApp.newTransaction)} />
         <div className="w-full px-10 lg:w-2/6">
 
           <h1 className="splashTxt mt-[50px]">
             ¡Envio exitoso!
           </h1>
           <img src={iconWallet} className=" ml-auto mr-auto my-20"></img>
-          <ButtonPrimary type="button" name="Regresar" status={StatusButton.Enabled} onClick={() => navigate(routesNamesApp.qrTransaction)} />
+          <ButtonPrimary type="button" name="Regresar" status={StatusButton.Enabled} onClick={() => navigate(routesNamesApp.newTransaction)} />
         </div>
       </div>
 
